f3bc86dc27d01b4b5cecf16f358e464a9792b68f
Damian Zsiros <damjeronzalez23@gmail.com>
2023-01-04 13:02:12 -0500

    add route at api of team info per id

--- api/index.js
+++ api/index.js
@@ -44,6 +44,15 @@ app.get('/teams', (ctx) => {
   return ctx.json(teams)
 })
 
+app.get('/teams/:id', (ctx) => {
+  const id = ctx.req.param('id')
+  const foundTeam = teams.find((team) => team.id === id)
+
+  return foundTeam
+    ? ctx.json(foundTeam)
+    : ctx.json({ message: 'Team not found' }, 404)
+})
+
 app.get('/static/*', serveStatic({ root: './' }))
 
 export default app
