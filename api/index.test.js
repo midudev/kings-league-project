b9fb906790ac855f4eb668b42df57fa83b764056
Jona <jonathanelian64@gmail.com>
2023-01-04 12:10:10 -0700

    Add tests for route /teams

--- api/index.test.js
+++ api/index.test.js
@@ -28,3 +28,67 @@ describe('Testing / route', () => {
     }
   })
 })
+
+describe('Testing /teams route', () => {
+  let worker
+
+  beforeAll(async () => {
+    worker = await unstableDev(
+      'api/index.js',
+      {},
+      { disableExperimentalWarning: true }
+    )
+  })
+
+  afterAll(async () => {
+    await worker.stop()
+  })
+
+  it('The teams should have all props', async () => {
+    const resp = await worker.fetch('/teams')
+    if (resp) {
+      const teams = await resp.json()
+
+      // verify the team have all props
+      teams.forEach((team) => {
+        expect(team).toHaveProperty('id')
+        expect(team).toHaveProperty('name')
+        expect(team).toHaveProperty('image')
+        expect(team).toHaveProperty('url')
+        expect(team).toHaveProperty('presidentId')
+        expect(team).toHaveProperty('channel')
+        expect(team).toHaveProperty('coach')
+        expect(team).toHaveProperty('socialNetworks')
+        expect(team).toHaveProperty('players')
+      })
+    }
+  })
+
+  it('Get /teams/1k should return team props', async () => {
+    const resp = await worker.fetch('/teams/1k')
+    if (resp) {
+      const team = await resp.json()
+
+      expect(team).toHaveProperty('id')
+      expect(team).toHaveProperty('name')
+      expect(team).toHaveProperty('image')
+      expect(team).toHaveProperty('url')
+      expect(team).toHaveProperty('presidentId')
+      expect(team).toHaveProperty('channel')
+      expect(team).toHaveProperty('coach')
+      expect(team).toHaveProperty('socialNetworks')
+      expect(team).toHaveProperty('players')
+    }
+  })
+
+  it('Get /teams/noexist should return 404 message missing team', async () => {
+    const resp = await worker.fetch('/teams/noexist')
+    if (resp) {
+      const errorMessage = await resp.json()
+
+      expect(errorMessage).toEqual({
+        message: 'Team not found'
+      })
+    }
+  })
+})
