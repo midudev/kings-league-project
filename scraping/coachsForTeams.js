834d89bacbdffc94647f76cabe4693ff198244c9
Damian Zsiros <damjeronzalez23@gmail.com>
2023-01-04 23:33:49 -0500

    add scraper for coachs from web  that update info

---
+++ scraping/coachsForTeams.js
@@ -0,0 +1,54 @@
+import * as cheerio from 'cheerio'
+import { writeDBFile, TEAMS } from '../db/index.js'
+
+const URLS = {
+  coachs: 'https://es.besoccer.com/competicion/info/kings-league/2023'
+}
+
+const INFO_COACHS_SELECTORS = {
+  teamName: { selector: '.name.mt10', typeOf: 'string' },
+  coach: { selector: '.name.mt20', typeOf: 'string' }
+}
+
+async function scrape(url) {
+  const res = await fetch(url)
+  const html = await res.text()
+  return cheerio.load(html)
+}
+
+function replaceFCOfTeamName(teamName) {
+  return teamName.replace(' FC', '')
+}
+
+async function getCoachsOfTeams() {
+  const $ = await scrape(URLS.coachs)
+  const coachsTeam = $(INFO_COACHS_SELECTORS.coach.selector)
+    .toArray()
+    .map((coachName) => coachName.children[0].data)
+  const teamsName = $(INFO_COACHS_SELECTORS.teamName.selector)
+    .toArray()
+    .map((teamName) => teamName.children[0].data)
+  const teamsWithCoach = coachsTeam.map((coach, i) => {
+    return { coach, teamName: replaceFCOfTeamName(teamsName[i]) }
+  })
+  return TEAMS.map((team) => {
+    const coachInfoTeam = teamsWithCoach.filter((teamWithCoach, i) => {
+      const teamWithCoachFormatted = replaceFCOfTeamName(
+        teamWithCoach.teamName.toLocaleUpperCase()
+      )
+      const teamFoundedFormatted = replaceFCOfTeamName(
+        team.name.toLocaleUpperCase()
+      )
+      return teamWithCoachFormatted === teamFoundedFormatted
+    })[0]
+
+    return {
+      ...team,
+      coach: coachInfoTeam.coach
+    }
+  })
+}
+
+const teamsInfoWithCoach = await getCoachsOfTeams()
+
+writeDBFile('teams', teamsInfoWithCoach)
