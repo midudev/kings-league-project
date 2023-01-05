fc04a6ea0981269bf3666372c2ee93f8713bd116
Miguel Ángel Durán <miduga@gmail.com>
2023-01-05 19:18:40 +0100

    Add new scraping utils to the file

--- scraping/utils.js
+++ scraping/utils.js
@@ -1,8 +1,19 @@
 import * as cheerio from 'cheerio'
 
-export const URLS = {
-	leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/',
-	mvp: 'https://kingsleague.pro/estadisticas/mvp/'
+import { getLeaderBoard } from './leaderboard.js'
+import { getMvpList } from './mvp.js'
+import { logError, logInfo, logSuccess } from './log.js'
+import { writeDBFile } from '../db/index.js'
+
+export const SCRAPINGS = {
+	leaderboard: {
+		url: 'https://kingsleague.pro/estadisticas/clasificacion/',
+		scraper: getLeaderBoard
+	},
+	mvp: {
+		url: 'https://kingsleague.pro/estadisticas/mvp/',
+		scraper: getMvpList
+	}
 }
 
 export const cleanText = (text) =>
@@ -15,4 +26,28 @@ export async function scrape(url) {
 	const res = await fetch(url)
 	const html = await res.text()
 	return cheerio.load(html)
+}
+
+export async function scrapeAndSave(name) {
+	const start = performance.now()
+
+	try {
+		const { scraper, url } = SCRAPINGS[name]
+
+		logInfo(`Scraping [${name}]...`)
+		const $ = await scrape(url)
+		const content = await scraper($)
+		logSuccess(`[${name}] scraped successfully`)
+
+		logInfo(`Writing [${name}] to database...`)
+		await writeDBFile(name, content)
+		logSuccess(`[${name}] written successfully`)
+	} catch (e) {
+		logError(`Error scraping [${name}]`)
+		logError(e)
+	} finally {
+		const end = performance.now()
+		const time = (end - start) / 1000
+		logInfo(`[${name}] scraped in ${time} seconds`)
+	}
 }
