7d4b7f5da7d308b4844f1daccede3328efb6f7a9
david hernandez <davidfelipehernandez1@gmail.com>
2023-01-05 15:26:43 -0500

    Move func getImageFromTeam for reusability

--- db/index.js
+++ db/index.js
@@ -3,17 +3,22 @@ import path from 'node:path'
 
 const DB_PATH = path.join(process.cwd(), './db/')
 
-function readDBFile(dbName) {
-  return readFile(`${DB_PATH}/${dbName}.json`, 'utf-8').then(JSON.parse)
+function readDBFile (dbName) {
+	return readFile(`${DB_PATH}/${dbName}.json`, 'utf-8').then(JSON.parse)
 }
 
 export const TEAMS = await readDBFile('teams')
 export const PRESIDENTS = await readDBFile('presidents')
 
-export function writeDBFile(dbName, data) {
-  return writeFile(
-    `${DB_PATH}/${dbName}.json`,
-    JSON.stringify(data, null, 2),
-    'utf-8'
-  )
+export function writeDBFile (dbName, data) {
+	return writeFile(
+		`${DB_PATH}/${dbName}.json`,
+		JSON.stringify(data, null, 2),
+		'utf-8'
+	)
+}
+
+export const getImageFromTeam = ({ name }) => {
+	const { image } = TEAMS.find((team) => team.name === name)
+	return image
 }
