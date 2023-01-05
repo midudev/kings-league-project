78479aeafe8c69467ec09e6eacea31946add4e4e
Miguel Ángel Durán <miduga@gmail.com>
2023-01-05 18:51:07 +0100

    Format file

--- scraping/mvp.js
+++ scraping/mvp.js
@@ -1,56 +1,47 @@
 import { writeDBFile, TEAMS } from '../db/index.js'
-import { URLS, scrape } from './utils.js'
+import { URLS, scrape, cleanText } from './utils.js'
+
+const MVP_SELECTORS = {
+	team: { selector: '.fs-table-text_3', typeOf: 'string' },
+	playerName: { selector: '.fs-table-text_4', typeOf: 'string' },
+	gamesPlayed: { selector: '.fs-table-text_5', typeOf: 'number' },
+	mvps: { selector: '.fs-table-text_6', typeOf: 'number' }
+}
 
 async function getMvpList() {
-  const $ = await scrape(URLS.mvp)
-  const $rows = $('table tbody tr')
+	const $ = await scrape(URLS.mvp)
+	const $rows = $('table tbody tr')
 
-  const MVP_SELECTORS = {
-    rank: { selector: '.fs-table-text_1', typeOf: 'number' },
-    team: { selector: '.fs-table-text_3', typeOf: 'string' },
-    playerName: { selector: '.fs-table-text_4', typeOf: 'string' },
-    gamesPlayed: { selector: '.fs-table-text_5', typeOf: 'number' },
-    mvps: { selector: '.fs-table-text_6', typeOf: 'number' }
-  }
+	const getImageFromTeam = ({ name }) => {
+		const { image } = TEAMS.find((team) => team.name === name)
+		return image
+	}
 
-  const getImageFromTeam = ({ name }) => {
-    const { image } = TEAMS.find(
-      (team) => team.name === name
-    )
-    return image
-  }
+	const mvpSelectorEntries = Object.entries(MVP_SELECTORS)
+	const mvpList = []
 
-  const cleanText = (text) =>
-    text
-      .replace(/\t|\n|\s:/g, '')
-      .replace(/.*:/g, ' ')
-      .trim()
+	$rows.each((index, el) => {
+		const mvpEntries = mvpSelectorEntries.map(([key, { selector, typeOf }]) => {
+			const rawValue = $(el).find(selector).text()
+			const cleanedValue = cleanText(rawValue)
 
-  const mvpSelectorEntries = Object.entries(MVP_SELECTORS)
-  const mvpList = []
-  $rows.each((_, el) => {
-    const mvpEntries = mvpSelectorEntries.map(
-      ([key, { selector, typeOf }]) => {
-        const rawValue = $(el).find(selector).text()
-        const cleanedValue = cleanText(rawValue)
+			const value = typeOf === 'number' ? Number(cleanedValue) : cleanedValue
 
-        const value = typeOf === 'number' ? Number(cleanedValue) : cleanedValue
+			return [key, value]
+		})
 
-        return [key, value]
-      }
-    )
-    const { team: teamName, ...mvpData } =
-		Object.fromEntries(mvpEntries)
-    const image = getImageFromTeam({ name: teamName })
+		const { team: teamName, ...mvpData } = Object.fromEntries(mvpEntries)
+		const image = getImageFromTeam({ name: teamName })
 
-    mvpList.push({
-      ...mvpData,
-      team: teamName,
-      image
-    })
-    // mvpList.push(Object.fromEntries(mvpEntries))
-  })
-  return mvpList
+		mvpList.push({
+			...mvpData,
+			ranking: index + 1,
+			team: teamName,
+			image
+		})
+	})
+
+	return mvpList
 }
 
 const mvpList = await getMvpList()
