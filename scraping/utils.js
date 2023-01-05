b9e2c1f70cec677dfa21c60a2fbdac75635a6310
Miguel Ángel Durán <miduga@gmail.com>
2023-01-05 18:51:13 +0100

    Add cleanText method

--- scraping/utils.js
+++ scraping/utils.js
@@ -1,12 +1,18 @@
 import * as cheerio from 'cheerio'
 
 export const URLS = {
-  leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/',
-  mvp: 'https://kingsleague.pro/estadisticas/mvp/'
+	leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/',
+	mvp: 'https://kingsleague.pro/estadisticas/mvp/'
 }
 
+export const cleanText = (text) =>
+	text
+		.replace(/\t|\n|\s:/g, '')
+		.replace(/.*:/g, ' ')
+		.trim()
+
 export async function scrape(url) {
-  const res = await fetch(url)
-  const html = await res.text()
-  return cheerio.load(html)
+	const res = await fetch(url)
+	const html = await res.text()
+	return cheerio.load(html)
 }
