f916430ca5ade1e2b45039427a298f913de91e50
Damian Zsiros <damjeronzalez23@gmail.com>
2023-01-06 01:51:10 -0500

    add execution of method in scrape manager

--- scraping/index.js
+++ scraping/index.js
@@ -1,5 +1,8 @@
+import { getURLTeams } from './url_teams.js'
 import { scrapeAndSave, SCRAPINGS } from './utils.js'
 
 for (const infoToScrape of Object.keys(SCRAPINGS)) {
 	await scrapeAndSave(infoToScrape)
 }
+
+getURLTeams()
