a8312d17cea0ab0c2e81e88420ba0feca9da14bf
Miguel Ángel Durán <miduga@gmail.com>
2023-01-06 18:28:09 +0100

    Format tests file

--- api/index.test.js
+++ api/index.test.js
@@ -2,105 +2,102 @@ import { unstable_dev as unstableDev } from 'wrangler'
 import { describe, expect, it, beforeAll, afterAll } from 'vitest'
 
 const setup = async () => {
-  const worker = await unstableDev(
-    'api/index.js',
-    {},
-    { disableExperimentalWarning: true }
-  )
-  return worker
+	const worker = await unstableDev('api/index.js', {}, { disableExperimentalWarning: true })
+
+	return worker
 }
 
 const teardown = async (worker) => {
-  await worker.stop()
+	await worker.stop()
 }
 
 describe('Testing / route', () => {
-  let worker
+	let worker
 
-  beforeAll(async () => {
-    worker = await setup()
-  })
+	beforeAll(async () => {
+		worker = await setup()
+	})
 
-  afterAll(async () => {
-    await teardown(worker)
-  })
+	afterAll(async () => {
+		await teardown(worker)
+	})
 
-  it('routes should have endpoint and description', async () => {
-    const resp = await worker.fetch()
-    expect(resp).toBeDefined()
-    if(!resp) return
+	it('routes should have endpoint and description', async () => {
+		const res = await worker.fetch()
+		expect(res).toBeDefined()
+		if (!res) return
 
-    const apiRoutes = await resp.json()
-    // verify the response to have the expected format
-    apiRoutes.forEach((endpoint) => {
-      expect(endpoint).toHaveProperty('endpoint')
-      expect(endpoint).toHaveProperty('description')
-    })
-  })
+		const apiRoutes = await res.json()
+		// verify the response to have the expected format
+		apiRoutes.forEach((endpoint) => {
+			expect(endpoint).toHaveProperty('endpoint')
+			expect(endpoint).toHaveProperty('description')
+		})
+	})
 })
 
 describe('Testing /teams route', () => {
-  let worker
+	let worker
 
-  beforeAll(async () => {
-    worker = await setup()
-  })
+	beforeAll(async () => {
+		worker = await setup()
+	})
 
-  afterAll(async () => {
-    await teardown(worker)
-  })
+	afterAll(async () => {
+		await teardown(worker)
+	})
 
-  it('The teams should have all teams', async () => {
-    const resp = await worker.fetch('/teams')
-    expect(resp).toBeDefined()
-    if(!resp) return
-    
-    const teams = await resp.json()
-    const numberTeams = Object.entries(teams).length
+	it('The teams should have all teams', async () => {
+		const resp = await worker.fetch('/teams')
+		expect(resp).toBeDefined()
+		if (!resp) return
 
-    // verify the team have all props
-    teams.forEach((team) => {
-      expect(team).toHaveProperty('id')
-      expect(team).toHaveProperty('name')
-      expect(team).toHaveProperty('image')
-      expect(team).toHaveProperty('url')
-      expect(team).toHaveProperty('presidentId')
-      expect(team).toHaveProperty('channel')
-      expect(team).toHaveProperty('coach')
-      expect(team).toHaveProperty('socialNetworks')
-      expect(team).toHaveProperty('players')
-    })
+		const teams = await resp.json()
+		const numberTeams = Object.entries(teams).length
 
-    expect(numberTeams).toBe(12)
-  })
+		// verify the team have all props
+		teams.forEach((team) => {
+			expect(team).toHaveProperty('id')
+			expect(team).toHaveProperty('name')
+			expect(team).toHaveProperty('image')
+			expect(team).toHaveProperty('url')
+			expect(team).toHaveProperty('presidentId')
+			expect(team).toHaveProperty('channel')
+			expect(team).toHaveProperty('coach')
+			expect(team).toHaveProperty('socialNetworks')
+			expect(team).toHaveProperty('players')
+		})
 
-  it('Get /teams/1k should return team props', async () => {
-    const resp = await worker.fetch('/teams/1k')
-    expect(resp).toBeDefined()
-    if(!resp) return
-  
-    const team = await resp.json()
+		expect(numberTeams).toBe(12)
+	})
 
-    expect(team).toHaveProperty('id')
-    expect(team).toHaveProperty('name')
-    expect(team).toHaveProperty('image')
-    expect(team).toHaveProperty('url')
-    expect(team).toHaveProperty('presidentId')
-    expect(team).toHaveProperty('channel')
-    expect(team).toHaveProperty('coach')
-    expect(team).toHaveProperty('socialNetworks')
-    expect(team).toHaveProperty('players')
-  })
+	it('Get /teams/1k should return team props', async () => {
+		const resp = await worker.fetch('/teams/1k')
+		expect(resp).toBeDefined()
+		if (!resp) return
 
-  it('Get /teams/noexist should return 404 message missing team', async () => {
-    const resp = await worker.fetch('/teams/noexist')
-    expect(resp).toBeDefined()
-    if(!resp) return
-    
-    const errorMessage = await resp.json()
+		const team = await resp.json()
 
-    expect(errorMessage).toEqual({
-      message: 'Team not found'
-    })
-  })
+		expect(team).toHaveProperty('id')
+		expect(team).toHaveProperty('name')
+		expect(team).toHaveProperty('image')
+		expect(team).toHaveProperty('url')
+		expect(team).toHaveProperty('presidentId')
+		expect(team).toHaveProperty('channel')
+		expect(team).toHaveProperty('coach')
+		expect(team).toHaveProperty('socialNetworks')
+		expect(team).toHaveProperty('players')
+	})
+
+	it('Get /teams/noexist should return 404 message missing team', async () => {
+		const resp = await worker.fetch('/teams/noexist')
+		expect(resp).toBeDefined()
+		if (!resp) return
+
+		const errorMessage = await resp.json()
+
+		expect(errorMessage).toEqual({
+			message: 'Team not found'
+		})
+	})
 })
