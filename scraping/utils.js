48bdf58664f5cbb2cf1abe13955219e5ccbd4874
Pabl0Parra <pabsblue16@gmail.com>
2023-01-06 10:09:33 +0100

    changed top_scorers file import@utils

--- scraping/utils.js
+++ scraping/utils.js
@@ -2,7 +2,7 @@ import * as cheerio from 'cheerio'
 
 import { getLeaderBoard } from './leaderboard.js'
 import { getMvpList } from './mvp.js'
-import { getTopScorersList } from './top_scorer.js'
+import { getTopScorersList } from './top_scorers.js'
 import { logError, logInfo, logSuccess } from './log.js'
 import { writeDBFile } from '../db/index.js'
 
