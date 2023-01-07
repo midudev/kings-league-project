8f86aebf8ea00fa4f7461a410e79b2d0bedf2166
Juan Ortiz <ortizjuandavid@gmail.com>
2023-01-06 23:26:34 -0500

    excluded files from test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	test: {
 		setupFiles: ['./global-setup/shared-mocks.js'],
+		exclude: [...configDefaults.exclude, 'astro.config.mjs', 'tailwind.config.cjs'],
 		coverage: {
+			exclude: [...configDefaults.coverage.exclude, 'astro.config.mjs', 'tailwind.config.cjs'],
 			all: true
 		}
 	}
