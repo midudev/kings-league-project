41f78d8b88c8e9bb19bb4da92dbe527dc0ad2b41
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-01-06 19:37:13 -0500

    :loud_sound: Logs messages on `getSchedule` function

--- scraping/schedule.js
+++ scraping/schedule.js
@@ -1,4 +1,5 @@
 import { cleanText } from './utils.js'
+import { logError, logInfo, logSuccess } from './log.js'
 import { writeDBFile } from '../db/index.js'
 import puppeteer from 'puppeteer'
 
@@ -24,49 +25,63 @@ export async function getSchedule() {
 	await page.goto(BASE_URL)
 	await page.waitForSelector(SELECTORS['match'])
 
+	const start = performance.now()
+	logInfo('Scrapping schedule...')
+
 	// Each day
 	const $matches = await page.$$(SELECTORS['match'])
 
 	for await (const $match of $matches) {
-		const day = []
+		try {
+			const day = []
 
-		const $date = await $match.$(SELECTORS['date'])
-		const dateRaw = await getTextFromElement($date, page)
-		const date = cleanText(dateRaw)
+			const $date = await $match.$(SELECTORS['date'])
+			const dateRaw = await getTextFromElement($date, page)
+			const date = cleanText(dateRaw)
+			logInfo(`Starting with day: [${date}]`)
 
-		const $locals = await $match.$$(SELECTORS['locals'])
-		const $visitants = await $match.$$(SELECTORS['visitants'])
-		const $scores = await $match.$$(SELECTORS['scores'])
+			const $locals = await $match.$$(SELECTORS['locals'])
+			const $visitants = await $match.$$(SELECTORS['visitants'])
+			const $scores = await $match.$$(SELECTORS['scores'])
 
-		// Each match of the day
-		let matchIndex = 0
+			// Each match of the day
+			let matchIndex = 0
 
-		for await (const $score of $scores) {
-			const $currentScore = $score
-			const $currentLocal = $locals[matchIndex]
-			const $currentVisitant = $visitants[matchIndex]
+			for await (const $score of $scores) {
+				const $currentScore = $score
+				const $currentLocal = $locals[matchIndex]
+				const $currentVisitant = $visitants[matchIndex]
 
-			const scoreRaw = await getTextFromElement($currentScore, page)
-			const score = cleanText(scoreRaw)
-			const localRaw = await getTextFromElement($currentLocal, page)
-			const local = cleanText(localRaw)
-			const visitantRaw = await getTextFromElement($currentVisitant, page)
-			const visitant = cleanText(visitantRaw)
+				const scoreRaw = await getTextFromElement($currentScore, page)
+				const score = cleanText(scoreRaw)
+				const localRaw = await getTextFromElement($currentLocal, page)
+				const local = cleanText(localRaw)
+				const visitantRaw = await getTextFromElement($currentVisitant, page)
+				const visitant = cleanText(visitantRaw)
 
-			if (score.includes('–')) {
-				day.push({ teams: [local, visitant], score })
-			} else {
-				day.push({ teams: [local, visitant], score: null })
+				if (score.includes('–')) {
+					day.push({ teams: [local, visitant], score })
+				} else {
+					day.push({ teams: [local, visitant], score: null })
+				}
+
+				matchIndex++
 			}
 
-			matchIndex++
+			logSuccess(`Day [${date}] was successfully scraped`)
+			schedule.push({ date, matches: day })
+		} catch (error) {
+			logError(`Error scraping [${date}] schedule`)
+			logError(error)
 		}
-
-		schedule.push({ date, matches: day })
 	}
 
 	await page.close()
 	await browser.close()
+
+	const end = performance.now()
+	const time = (end - start) / 1000
+	logSuccess(`Schedule was succesfully scraped in [{time}] seconds`)
 
 	return schedule
 }
