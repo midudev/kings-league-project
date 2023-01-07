32aae8d8dcc8d50ead68033f6dbee5e4a0228e40
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-01-06 20:45:59 -0500

    :sparkles: Add team id on `schedule.json`
    Get team id while scraping

--- scraping/schedule.js
+++ scraping/schedule.js
@@ -4,7 +4,9 @@ const SELECTORS = {
 	match: '#calendarMatch',
 	date: '.el-table-title',
 	locals: '.el-text-1',
+	localsImages: '.fs-table-text_3 img',
 	visitants: '.el-text-7',
+	visitantsImages: '.fs-table-text_5 img',
 	scores: '.fs-table-text_8'
 }
 
@@ -20,21 +22,30 @@ export async function getSchedule($) {
 		const date = cleanText(dateRaw)
 
 		const $locals = $day.find(SELECTORS['locals'])
+		const $localsImages = $day.find(SELECTORS['localsImages'])
 		const $visitants = $day.find(SELECTORS['visitants'])
+		const $visitantsImages = $day.find(SELECTORS['visitantsImages'])
 		const $results = $day.find(SELECTORS['scores'])
 
 		$results.each((index, result) => {
 			const scoreRaw = $(result).text()
 			const score = cleanText(scoreRaw)
 
-			const localRaw = $($locals[index]).text()
-			const local = cleanText(localRaw)
+			const localNameRaw = $($locals[index]).text()
+			const localName = cleanText(localNameRaw)
+			const localImg = $($localsImages[index]).attr('src')
+			const localId = localImg.replace('/wp-content/uploads/2022/11/', '').replace('.svg', '')
 
-			const visitantRaw = $($visitants[index]).text()
-			const visitant = cleanText(visitantRaw)
+			const visitantNameRaw = $($visitants[index]).text()
+			const visitantName = cleanText(visitantNameRaw)
+			const visitantImg = $($visitantsImages[index]).attr('src')
+			const visitantId = visitantImg.replace('/wp-content/uploads/2022/11/', '').replace('.svg', '')
 
 			matches.push({
-				teams: [local, visitant],
+				teams: [
+					{ id: localId, name: localName },
+					{ id: visitantId, name: visitantName }
+				],
 				score
 			})
 		})
