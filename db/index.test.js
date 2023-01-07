721e96884dd650a531d416ca719d88f8ed70648c
Fernando Cases <nandocasesgarcia@gmail.com>
2023-01-07 22:16:16 +0100

    fix: fix db test

--- db/index.test.js
+++ db/index.test.js
@@ -5,12 +5,17 @@ describe('testing db functionality', () => {
 	it('saves data to JSON file', () => {
 		writeDBFile('dummy', { data: 'dummy' })
 	})
-	it('teams  and presidents haves values', () => {
+	it('teams and presidents have values', () => {
 		expect(TEAMS).toBeDefined()
 		expect(PRESIDENTS).toBeDefined()
 	})
-	it('returns team image', () => {
+	it('returns correct team image', () => {
 		const image = getImageFromTeam({ name: '1K FC' })
 		expect(image).toBe('https://api.kingsleague.dev/static/logos/1k.svg')
 	})
+	it('returns default image for unknown team', () => {
+		const image = getImageFromTeam({ name: 'Unknown Team' })
+		expect(image).toBe('https://api.kingsleague.dev/static/logos/default.svg')
+	})
 })
+
