614f6966d900849343630d0ba887d8c81246e99d
Damian Zsiros <damjeronzalez23@gmail.com>
2023-01-07 23:17:10 -0500

    update year for date of 2022 at year per time

--- api/index.js
+++ api/index.js
@@ -183,8 +183,9 @@ app.get('/schedule/:month', (ctx) => {
 	const month = ctx.req.param('month')
 	const schedulePerMonth = schedule.filter((day) => {
 		const infoDate = day.date.split(' ').filter((info) => info !== 'de')
+		const actualYear = new Date().getFullYear()
 		const monthInSpanish = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(
-			new Date(2022, month, 1)
+			new Date(actualYear, month, 1)
 		)
 
 		return infoDate[1].toLocaleLowerCase() === monthInSpanish
